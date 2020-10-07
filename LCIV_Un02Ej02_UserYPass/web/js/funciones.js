ac7f404043032214dfbc62ca5b85013be2751059
AngelZaragoza <111610@tecnicatura.frc.utn.edu.ar>
2020-10-07 10:37:02 -0300

    Unidad 02 Ejer 02 - cambiadas validaciones y css

--- LCIV_Un02Ej02_UserYPass/web/js/funciones.js
+++ LCIV_Un02Ej02_UserYPass/web/js/funciones.js
@@ -14,12 +14,14 @@ function saludo() {
     document.getElementById("saludo").innerHTML = texto;
 }
 
+//Esta función muestra alertas con textos de validación
 function validarUP () {
     let user = document.getElementsByName("user")[0];
     let pass = document.getElementsByName("pass")[0];
     
     if (user.value.length == 0) {
         alert('Debe ingresar un Nombre de Usuario');
+        if (document.getElementsByTagName())
         user.focus();
         return false;
     }
@@ -39,4 +41,53 @@ function validarUP () {
     }
     
     return true;
+}
+
+//Esta función llama a otra en caso de que las validaciones fallen
+function validarForm () {
+    let user = document.getElementsByName("user")[0];
+    let pass = document.getElementsByName("pass")[0];
+    let valida = 1;
+    
+    document.getElementById("lista").innerHTML = "";
+    
+    if (user.value.length == 0) {
+        textoValidacion("Debe ingresar un Nombre de Usuario");        
+        valida -= 1;
+    }
+    
+    if (pass.value.length == 0) {
+        textoValidacion("Debe ingresar un Password");        
+        valida -= 2;
+    }
+    
+    //Si cualquiera de los dos if anteriores da true, el sgte. if retorna false
+    
+    if (valida == -1) {
+        pass.focus();
+        return false;
+    } else if (valida <= 0) {
+        user.focus();
+        return false;
+    }
+
+    if (user.value == "tecni" && pass.value == "laboratorio") {
+        alert('Sesión iniciada con éxito!');        
+    } else {
+        textoValidacion("Usuario o Password incorrecto");
+        user.focus();
+        return false;
+    }
+    
+    return true;
+}
+
+//Esta función inserta elementos en una lista con textos de validación
+function textoValidacion(texto) {
+    let elemento = document.createElement("li");
+    let nodo = document.createTextNode(texto);
+    let padre = document.getElementById("lista");
+    
+    elemento.appendChild(nodo);
+    padre.appendChild(elemento);
 }
